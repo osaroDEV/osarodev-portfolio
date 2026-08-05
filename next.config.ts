3881ebb808a7f4977d70cdc3da80a084606f23c8
osaroDEV <osernon@gmail.com>
2026-08-05 19:18:35 +0100

    add sub-domain

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,20 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  async rewrites() {
+    return [
+      {
+        source: '/coding-app',
+        // TODO: Replace this destination with the actual Vercel URL of your html-tutor-app
+        destination: 'https://code-illustrated.vercel.app/coding-app',
+      },
+      {
+        source: '/coding-app/:path*',
+        // TODO: Replace this destination with the actual Vercel URL of your html-tutor-app
+        destination: 'https://code-illustrated.vercel.app/coding-app/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
